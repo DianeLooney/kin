696dccf4657b79bda892bb595bbdc545c8ecbc9d
Diane Looney <diane@dianelooney.com>
2019-12-10 21:20:10 -0500

    Add list comprehension stuff

--- pkg/compiler/js/runtime.go
+++ pkg/compiler/js/runtime.go
@@ -11,6 +11,11 @@ const runtime = {
   floor: x => Math.floor(x),
   map: fn => arr => arr.map(fn),
   reduce: fn => init => arr => arr.reduce((x,y) => fn(y)(x), init),
+  
+  init: arr => arr.slice(0, arr.length - 1),
+  head: arr => arr[0],
+  tail: arr => arr.slice(1),
+  last: arr => arr[arr.length - 1],
 };
 const handler = {
   has: () => true,
